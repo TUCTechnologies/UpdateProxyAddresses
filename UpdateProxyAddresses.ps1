810c9698e3118b6f904096067305b4c60a04390e
Brandon <brandon@willjasen.com>
2023-07-31 12:12:42 -0400

    refactoring

--- UpdateProxyAddresses.ps1
+++ UpdateProxyAddresses.ps1
@@ -1,6 +1,6 @@
 # When set to $true, then no actions will be taken, only tested
 # When set to $false, script will perform actions
-$WhatIfPreference = $false
+$WhatIfPreference = $true
 If($WhatIfPreference -eq $true) {
 	$host.ui.RawUI.ForegroundColor = "Green"
 	Write-Host "--- Script is in testing mode, no actions will be taken ---" -ForegroundColor Blue
@@ -32,75 +32,58 @@ Function updateProxyAddress {
 	
 	Param (
 		[Microsoft.ActiveDirectory.Management.ADUser]$user,
-		[string]$addressToAdd,
-		[bool]$primaryAddress
+		[string]$addressToAdd
 	)
 	
 	# Check if the address has already been added
 	$alreadyAdded = 0
 	ForEach($userAddress in $user.proxyAddresses) {
-		If($primaryAddress) {
-			If(("SMTP:"+$addressToAdd) -eq $userAddress) {
-				$alreadyAdded = 1
-				WriteLog ($user.samAccountName + " already has a primary proxy address of: " + $addressToAdd)
-				break
-			}
-		}
-		Else {
-			If(("smtp:"+$addressToAdd) -eq $userAddress) {
-				$alreadyAdded = 1
-				WriteLog ($user.samAccountName + " already has a proxy address of: " + $addressToAdd)
-				break
-			}
+		If($addressToAdd -eq $userAddress) {
+			$alreadyAdded = 1
+			WriteLog ($user.samAccountName + " already has a proxy address of: " + $addressToAdd)
+			break
 		}
 	}
 	# If the address hasn't been added, do so
 	If($alreadyadded -eq 0) {
-		If($primaryAddress) {
-			Set-ADUser -Identity $user.samAccountName -add @{proxyAddresses=("SMTP:"+$addressToAdd)}
-			WriteLog ($user.samAccountName + " now has a primary proxy address of: " + $addressToAdd)
-		}
-		Else {
-			Set-ADUser -Identity $user.samAccountName -add @{proxyAddresses=("smtp:"+$addressToAdd)}
-			WriteLog ($user.samAccountName + " now has a proxy address of: " + $addressToAdd)
-		}
+		Set-ADUser -Identity $user.samAccountName -add @{proxyAddresses=($addressToAdd)}
+		WriteLog ($user.samAccountName + " now has a proxy address of: " + $addressToAdd)
 	}
 }
 
-$Users = Import-Csv -Path ".\ADSync.csv"
+$Users = Import-Csv -Path ".\users.csv"
 $upnSuffixes = Get-ADForest | Select UPNSuffixes -ExpandProperty UPNSuffixes
 ForEach ($User in $Users) {
 	
-	# Get the AD account
-	$ADUser = Get-ADUser -Identity $User.username -Properties proxyAddresses
+	$UPN = ($User."User principal name")
+	$username = $UPN.split("@")[0]
+	$upnSuffix = $UPN.split("@")[1]
 	
-	# Update the UPN
-	# Make sure the UPN to change to has an existing UPN suffix
-	$suffixFound = $false
-	$suffixToCheck = $User.domain	
-	ForEach($suffix in $upnSuffixes) {
-		If($suffix -eq $suffixToCheck) { $suffixFound = $true; break }
-	}
-	
-	If([bool][int]$User.primaryAddress -eq $true) {
-		If($suffixFound -eq $false) {
-			WriteLog ("The suffix of '$suffixToCheck' does not exist, " + $User.username + " will not be changed")
+	# Get the user from Active Directory if it exists, move on if it doesn't
+	$ADUser = Try { 
+			Get-ADUser -Identity $username -Properties proxyAddresses
+		} 
+		Catch {
+			WriteLog ("The username '$username' does not exist in Active Directory... continuing.")
 			Continue
 		}
-		
-		$UPN = $User.username + "@" + $User.domain
-		If($ADUser.UserPrincipalName -ne $UPN) {
-			Set-ADUser -Identity $User.username -UserPrincipalName $UPN
-			WriteLog ($User.username + " now has UPN of: " + $UPN)
-		}
+	
+	# Update the UPN if needed
+	If($ADUser.UserPrincipalName -ne $UPN) {
+		Set-ADUser -Identity $username -UserPrincipalName $UPN
+		WriteLog ($username + " now has UPN of: " + $UPN)
 	}
 	
-	# Update proxy addresses if needed and the suffix exists
-	If($suffixFound) {
-		updateProxyAddress $ADUser $User.proxyAddress ([int]$User.primaryAddress)
-	}
-	Else {
-		WriteLog "The suffix of " + $suffixToCheck + " doesn't exist, not adding the proxy address of: " + $User.proxyAddress
+	# Update AD account with proxy addresses
+	$proxyAddressesToUpdate = ($User."Proxy addresses").split("+")
+	ForEach($proxyAddress in $proxyAddressesToUpdate) {
+		If($proxyAddress -like "*onmicrosoft.com*") {
+			# skip the addresses with onmicrosoft.com
+			Continue
+		}
+		Else {
+			updateProxyAddress $ADUser $proxyAddress
+		}
 	}
 }
 
